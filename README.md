13d2080f38c9f2e3317e318aa400b6dfa23567b8
zacanbot <zacanbot@gmail.com>
2024-12-23 15:07:51 -0700

    add ocr

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ LLM-based metadata extraction for government documents
 
 ## Setup
 
-Pull the repository and install python dependencies:
+Git and Python 3.10+ are required. Pull the repository and install python dependencies:
 
 ```bash
 git clone https://github.com/scholarsportal/gov-docs.git
@@ -20,12 +20,34 @@ Create a `.env` file with the following content:
 API_KEY = <your-Ollama-api-key>
 ```
 
-## Usage
+## 1. Download Internet Archive Documents
 
-Put all the text files to be processed in the `input` folder and run the script:
+Create or edit the csv file containing the Internet Archive download links in the `docs` folder. Then run the downloader:
 
 ```bash
-python process.py input --force --debug
+python get_ia_files.py docs/govdocs_1.csv 10
+```
+
+The optional number parameter after the input csv file indicates the maximum number of documents to be downloaded.
+
+## 2. OCR
+
+You can use the `ocr_pdf.py` script to perform OCR on a pdf file. This script uses Tesseract to detect text in a pdf and then saves it to a text file. If your pdf files are located in the `docs` folder, the script can be executed like this:
+
+```bash
+python ocr_pdf.py docs --dpi 200 --contrast 0.7 --lang eng --debug --force
+```
+
+The *debug* flag will limit processing to 30 pages. The *dpi* setting controls the resolution of the images used for OCR. Try to match the input document resolution. The *contrast* setting is a multiplier that can be used to adjust the contrast of the image before OCR. Use higher values if some text is not detected and lower values if too much text is detected. If you are getting lots of garbage text, try lowering the contrast to 0.8 or even 0.7. The *lang* setting controls the language used for OCR. You can use multiple languages separated by a + sign, e.g., `--lang eng+fra` (default). Normally, the script will not redo the OCR if a .txt file already exists. Using the `--force` parameter will override this behaviour.
+
+The output will be saved as text files in the `text` folder.
+
+## 3. Extract Metadata
+
+Ensure that all the text files to be processed are in the `text` folder and then run the script:
+
+```bash
+python process.py text --force --debug
 ```
 
 The `--force` parameter will ensure that embeddings are recreated. By default existing embeddings are kept. The output will be written to a json file named `metadata.json`.
--- ocr_pdf.py
+++ ocr_pdf.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+from pathlib import Path
 import cv2
 import numpy as np
 from pdf2image import convert_from_path
@@ -8,6 +9,7 @@ from concurrent.futures import ProcessPoolExecutor
 from PIL import Image, ImageOps, ImageEnhance
 
 DEBUG = False
+FORCE = False
 DPI = 150 # Try to match source document resolution
 CONTRAST = 0.9 # lower to remove noise, higher than one to increase contrast
 LANG = "eng+fra"
@@ -42,40 +44,59 @@ def ocr_page(args):
 
 
 def ocr_pdf(input_path):
-  print("Extracting images from pdf...")
-  if DEBUG: # Extract only the first 30 images
-    images = convert_from_path(input_path, dpi=DPI, first_page=1, last_page=30)
-  else: # Extract all images
-    images = convert_from_path(input_path, dpi=DPI)
-  print(f"{len(images)} pages found. Starting OCR.")
-  with ProcessPoolExecutor(max_workers=MAX_WORKERS) as executor:
-    ocr_texts = list(executor.map(ocr_page, enumerate(images)))
+  input_path = Path(input_path)
+  if input_path.is_file():
+    files = [input_path]
+  else:
+    if input_path.is_dir():
+      files = list(input_path.rglob("*.pdf"))
+    else:
+      raise FileNotFoundError(f"Could not find {input_path}")
+  # get total number of files
+  num_files = len(files)
+  print(f"Found {num_files} pdf files in {input_path}")
+  current_file = 0
+  for file in files:
+    current_file += 1
+    filepath = file.resolve()
+    output_dir = filepath.parent.parent / "text"
+    output_dir.mkdir(parents=True, exist_ok=True)
+    output_txt_path = output_dir / (filepath.stem + ".txt")
+    # Skip if text file already exists
+    if output_txt_path.exists() and not FORCE:
+      print(f"Text file {output_txt_path} already exists. Skipping.")
+      continue
+    print(f"Extracting images from {file.name} ({current_file} of {num_files}) ...")
+    if DEBUG: # Extract only the first 30 images
+      images = convert_from_path(filepath, dpi=DPI, first_page=1, last_page=30)
+    else: # Extract all images
+      images = convert_from_path(filepath, dpi=DPI)
+    print(f"{len(images)} pages found. Starting OCR.")
+    with ProcessPoolExecutor(max_workers=MAX_WORKERS) as executor:
+      ocr_texts = list(executor.map(ocr_page, enumerate(images)))
 
-  output_txt_path = os.path.splitext(input_path)[0] + ".txt"
-  with open(output_txt_path, 'w') as f:
-    for page_num, text in ocr_texts:
-      f.write(f"Page {page_num}:\n{text}\n\n")  # Write page number and OCR text to the file
-
-  print(f"OCR completed and saved to {output_txt_path}")
+    with open(output_txt_path, 'w') as f:
+      for page_num, text in ocr_texts:
+        f.write(f"Page {page_num}:\n{text}\n\n")  # Write page number and OCR text to the file
+    print(f"OCR completed and saved to {output_txt_path}")
 
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(
       description="Process images from input folder and save to output folder.")
-  parser.add_argument("input_path", type=str, help="Path to the input folder containing images")
+  parser.add_argument("input_path", type=str, help="Path to the input folder containing pdf files")
   parser.add_argument("--dpi", type=int, help="DPI setting for pdf OCR (optional)")
   parser.add_argument("--contrast", type=float, help="less than 1.0 to remove faint noise (optional)")
   parser.add_argument("--lang", type=str, default='eng+fra', help="Tesseract language string for OCR (optional)")
   parser.add_argument("--debug", action='store_true', help="Enable debug mode (optional)")
+  parser.add_argument("--force", action='store_true', help="Process OCR even if text file exist already (optional)")
   args = parser.parse_args()
   DEBUG = args.debug
+  FORCE = args.force
   if args.dpi is not None:
     DPI = args.dpi
   if args.contrast is not None:
     CONTRAST = args.contrast
   if args.lang is not None:
     LANG = args.lang
-  if not os.path.isfile(args.input_path):
-    print(f"Error: File '{args.input_path}' not found.")
-  else:
-    ocr_pdf(input_path=args.input_path)
+  ocr_pdf(input_path=args.input_path)
